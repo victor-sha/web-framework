8ee04b2ba4b9b67a6e0ebede1a15f15af1ef2719
victor-sha <kassini_90@mail.ru>
2019-08-27 17:18:27 +0300

    add events and json-server

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import { User } from './models/User';
 const user = new User({ name: 'John', age: 23 });
 user.set({ name: 'Join3' });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('hello', () => {
+  console.log('Hello event');
+});
+
+user.trigger('hello');

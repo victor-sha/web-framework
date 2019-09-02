bdfeaa730bdf65d1eab6b9aecedc1631368f8aa7
victor-sha <kassini_90@mail.ru>
2019-09-02 18:45:39 +0300

    fix this binding issues with arrow functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import { User } from './models/User';
 
 const user = new User({name: 'ne', age: 44});
+
+console.log(
+
+user.get('name')
+);
+
+
+user.on('change', () => {
+    console.log('user was changed');
+});
+
+user.trigger('change');
